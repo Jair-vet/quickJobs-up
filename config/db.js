634ea1edd20e451bcebd39ce2cb5e8450ee79a5a
Jair <jairaceves56@gmail.com>
2023-07-18 19:48:07 -0600

    Update

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
 // Conectando la base de datos usando el cluster de Mongoose
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
 
 // En caso de que exista un error de conexión
